5dd4ac2aa9306ca7f466b9cb7407461c74070230
bu-luochen <wangzuohaoc@163.com>
2025-11-17 23:40:18 +0800

    PID速度环调试

--- User/PID.c
+++ User/PID.c
@@ -2,20 +2,18 @@
 
 #include "PID.h"
 
+
+
 void PID_Update(PID_TypeDef *p)
 {
+	p->error2 = p->error1;
 	p->error1 = p->error0;
 	p->error0 = p->Target - p->Actual;
 	
-	if(p->Ki != 0){
-		p->errorInt += p->error0;
-	} else {
-		p->errorInt = 0;
-	}
+	p->Out += p->Kp * (p->error0 - p->error1)
+		   + p->Ki * p->error0 
+		   + p->Kd * (p->error0 - 2 * p->error1 + p->error2);
 	
-	p->Out = p->Kp * p->error0 
-		   + p->Ki * p->errorInt 
-		   + p->Kd * (p->error0 - p->error1);
 	if(p->Out > p->OutMax){p->Out = p->OutMax;}
 	if(p->Out < p->OutMin){p->Out = p->OutMin;}
 }	
--- User/main.c
+++ User/main.c
@@ -19,23 +19,38 @@
 
 extern  uint16_t Time_Serial;
 
-
+void PID_Set(uint8_t Index);
 
 PID_TypeDef left = {
+	
+	.error0 = 0,
+	.error1 = 0,
+	.error2 = 0,
+	
 	.Kp = 0.3,
-	.Ki = 0,
-	.Kd = 0,
+	.Ki = 0.1,
+	.Kd = 0.1,
+	
 	.OutMax = 100,
 	.OutMin = -100,
+	
+	.Out = 0,
 	
 };
 
 PID_TypeDef right = {
+	.error0 = 0,
+	.error1 = 0,
+	.error2 = 0,
+	
 	.Kp = 0.3,
-	.Ki = 0,
-	.Kd = 0,
+	.Ki = 0.1,
+	.Kd = 0.1,
+	
 	.OutMax = 100,
 	.OutMin = -100,
+	
+	.Out = 0,
 };
 
 
@@ -84,15 +99,44 @@ int main ()
 	
 	
 	
+
+	
+	
 	
 	while(1)
 	{	
+		
+		if(Serial_RxFlag == 1){
+				left.Target = 0;
+				right.Target = 0;
+				if(Serial_RxPacket[0] == '-'){
+					for(int i = 1;Serial_RxPacket[i] != '%';i++){
+						left.Target = left.Target * 10 + (Serial_RxPacket[i] - '0');
+					}
+					if(left.Target >= 100){left.Target = 100;}
+					left.Target = -left.Target;
+					right.Target = left.Target;
+				} else{  
+					for(int i = 0;Serial_RxPacket[i] != '%';i++){
+						left.Target = left.Target * 10 + (Serial_RxPacket[i] - '0');
+					}
+					if(left.Target >= 100){left.Target = 100;}
+					right.Target = left.Target;
+				}
+				
+			Serial_RxFlag = 0;
+				
+		}
+		
+		
+		
 		if(Key_Check(KEY_4,KEY_SINGLE)){
 			if(mode != 1){
 				up();
 				} else if(mode == 1 && menuIndex == 1)
 				{
 					menu[1].item[itemIndex].value += 0.01;
+					PID_Set(itemIndex);
 				}
 		}
 		
@@ -102,6 +146,7 @@ int main ()
 			} else if(mode == 1 && menuIndex == 1)
 			{
 				menu[1].item[itemIndex].value -= 0.01;
+				PID_Set(itemIndex);
 			}
 		}
 		
@@ -121,12 +166,14 @@ int main ()
 			if(mode == 1 && menuIndex == 1)
 			{
 				menu[1].item[itemIndex].value += 0.05;
+				PID_Set(itemIndex);
 			}
 		}
 		if(Key_Check(KEY_2,KEY_REPEAT)){
 			if(mode == 1 && menuIndex == 1)
 			{
-				menu[1].item[itemIndex].value -= 0.05;	
+				menu[1].item[itemIndex].value -= 0.05;
+				PID_Set(itemIndex);				
 			}
 		}
 		OLED_Clear();
@@ -134,6 +181,8 @@ int main ()
 			OLED_ShowString(120,0,"E",OLED_8X16);
 		}
 		
+		OLED_ShowNum(64,16,left.Target,3,OLED_8X16);
+		OLED_ShowNum(64,32,right.Target,3,OLED_8X16);
 		OLED_ShowMenu();
 		
 		
@@ -147,32 +196,61 @@ void TIM2_IRQHandler(void)
 	if(TIM_GetITStatus(TIM2,TIM_IT_Update)==SET){
 
 		Key_Tick();
-//		Serial_Tick();
-		Count1 ++;
-		if(Count1 >= 20){
-			Count1 = 0;
-			Speed = Encoder_Get();
-			
-			left.Actual = Speed;
-			
-			PID_Update(&left);
-			
-			Motor_SetSpeed(M1,left.Out);
+		Serial_Tick();
+		if(mode == 2){
+			Count1 ++;
+			if(Count1 >= 20){
+				Count1 = 0;
+				Speed = EI_GetTim3() * 100 / 142;
 				
+				left.Actual = Speed;
+				
+				PID_Update(&left);
+				
+				Motor_SetSpeed(M1,left.Out);
+					
+			}
+			
+			Count2++;
+			if(Count2 >= 20){
+				Count2 = 0;
+				Speed = EI_GetTim4() * 100 / 142;
+				
+				PID_Update(&right);
+				
+				Motor_SetSpeed(M2,right.Out);
+				
+			}
+			
+		} else {
+			Motor_SetSpeed(M1,0);
+			Motor_SetSpeed(M2,0);
 		}
 		
-		Count2++;
-		if(Count2 >= 20){
-			Count2 = 0;
-			right.Actual = Location;
-			
-			PID_Update(&right);
-			
-			Motor_SetSpeed(M2,right.Out);
-			
-		}
 		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
 	}
 	
 }
+
+
+
+void PID_Set(uint8_t Index)
+{
+	switch(Index){
+		case 0:
+				left.Kp = menu[1].item[itemIndex].value;
+				right.Kp = menu[1].item[itemIndex].value;
+			break;
+		case 1:
+				left.Ki = menu[1].item[itemIndex].value;
+				right.Ki = menu[1].item[itemIndex].value;
+			break;
+		case 2:
+				left.Kd = menu[1].item[itemIndex].value;
+				right.Kd = menu[1].item[itemIndex].value;
+			break;
+	}
+}
+
+
 
